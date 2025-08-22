7e84f70e22577dcdac5e517f70204e1e989467ee
jieny <576420147@qq.com>
2025-08-22 15:08:15 +0800

    移除 capacitor

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import pages from 'vite-plugin-pages'
-import { Capacitor } from '@capacitor/core'
 import path from 'node:path'
 
-const __IS_WEB__ = Capacitor.getPlatform() === 'web'
-const __IS_ANDROID__ = Capacitor.getPlatform() === 'android'
-const __IS_IOS__ = Capacitor.getPlatform() === 'ios'
-const __IS_NATIVE__ = JSON.stringify(Capacitor.isNativePlatform())
+// const __IS_WEB__ = Capacitor.getPlatform() === 'web'
+// const __IS_ANDROID__ = Capacitor.getPlatform() === 'android'
+// const __IS_IOS__ = Capacitor.getPlatform() === 'ios'
+// const __IS_NATIVE__ = JSON.stringify(Capacitor.isNativePlatform())
 
 export default defineConfig({
 	plugins: [react(), pages()],
@@ -19,10 +18,10 @@ export default defineConfig({
 	},
 	server: {},
 	define: {
-		__IS_WEB__,
+		__IS_WEB__: true,
 		__IS_ELECTRON__: false,
-		__IS_ANDROID__,
-		__IS_IOS__,
-		__IS_NATIVE__
+		// __IS_ANDROID__,
+		// __IS_IOS__,
+		// __IS_NATIVE__
 	}
 })
